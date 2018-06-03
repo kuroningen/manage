f7b69edee8bb40eef0674265479e3b8eb6c24d3a
黒人間 <kuroningen@ano.nymous.xyz>
2018-06-03 15:09:05 +0800

    Preparing login page based on admin-lte (not yet finished)

--- project/mvc/controller/contLogin.php
+++ project/mvc/controller/contLogin.php
@@ -1,12 +1,15 @@
 <?php namespace project\mvc\controller;
 
 /**
- * contLogin
+ * Controller responsible for authenticating the admin
  * @author  黒人間 <kuroningen@ano.nymous.xyz>
  * @package project\mvc\controller
  * @since   2018.06.03
  */
-class contLogin
+class contLogin extends contBase
 {
-
+    public function showLoginPage()
+    {
+        return view('template.admin-lte.login.login');
+    }
 }
--- project/mvc/view/template/main.blade.php
+++ project/mvc/view/template/main.blade.php
@@ -1,1 +1,8 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('main-title')</title>
+    @yield('main-head')
+</head>
+@yield('main-body')
+</html>
